4ba6a018a444a08c3d69daf4afa25237f90af2c8
NERVUser <123287164+NERVUser@users.noreply.github.com>
2023-04-27 13:52:02 -0400

    Add files via upload

---
+++ Movie.cpp
@@ -0,0 +1,14 @@
+#include "Movie.h"
+using namespace std;
+
+Movie::Movie() {
+	title = "";
+	year = "";
+	popularity = "";
+	rating = "";
+	revenue = "";
+}
+
+void Movie::Print() {
+	
+}
---
+++ Search.cpp
@@ -0,0 +1,13 @@
+#include "Search.h"
+using namespace std;
+
+Search::Search(string inputYear)
+{
+	for (auto itr = searchMovies.movieList.begin(); itr != searchMovies.movieList.end(); ++itr)
+	{
+		if (itr->year == inputYear)
+		{
+			searchmovieList.push_back(*itr);
+		}
+	}
+}
